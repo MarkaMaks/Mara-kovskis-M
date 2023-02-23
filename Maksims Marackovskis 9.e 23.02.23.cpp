b58872ccda217bd1d299a1561ef75a26f8bb161a
MarkaMaks <122529439+MarkaMaks@users.noreply.github.com>
2023-02-23 13:11:21 +0200

    23.02.2023
    Maksims Marackovskis 9.e

---
+++ Maksims Marackovskis 9.e 23.02.23.cpp
@@ -0,0 +1,60 @@
+﻿//#include <iostream>
+//#include <vector>
+//using namespace std;
+//int main() {
+//
+//	vector <int> num{ 1,2,3,1,2 };
+//	cout << num[0] << num[1] << num[2] << num[3] << num[4];
+//	if (num[0] == 1) cout << endl;
+//	if (num[1] == 1) cout << endl;
+//	if (num[2] == 1) cout << endl;
+//	if (num[3] == 1) cout << endl;
+//	if (num[1] == 1) cout << endl;
+//	if (num[2] == 1) cout << endl;
+
+		
+
+
+//}
+
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//int main() {
+//	vector <string> code{ "+1", "+44", "+86","+91", "+1", "+61" };
+//	vector <string> country{ "USA","UK", "CHINA", "INDIA", "CANADA", "AUSTRALIJA" };
+//	string num, cod;
+//	cout << "1. tel num, 2 kods" << endl;
+//	cin >> num >> cod;
+//	if (code[0] == cod) cout << country[0];
+//	if (code[1] == cod) cout << country[1];
+//	if (code[2] == cod) cout << country[2];
+//	if (code[3] == cod) cout << country[3];
+//	if (code[4] == cod) cout << country[4];
+//	if (code[5] == cod) cout << country[5];
+//	cout << cod << num;
+//
+//}
+
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//int main() {
+//	int cntr;
+//	vector <int> atz{ 3,8, 7, 4 };
+//	vector <string> name{ "Anna", "Antons", "Vlads", "Artemijs" };
+//	cout << "Enter atzīme:";
+//	cin  >> cntr;
+//	cout << endl;
+//
+//	if (atz[0] == cntr) { cout << atz[0] << endl; }
+//
+//	if (atz[1] == cntr) { cout << atz[1] << endl; }
+//
+//	if (atz[2] == cntr) { cout << atz[2] << endl; }
+//    if (atz[3] == cntr) { cout << atz[3] << endl; }
+//
+//	if (atz[0] != cntr && atz[1] != cntr && atz[2] != cntr && atz[3] != cntr) { cout << "Invalid name"; }
+//
+//}
+
